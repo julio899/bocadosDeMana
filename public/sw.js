6a77bbc09295fa97684544286627e1bf0db36ddd
Julio Vinachi <julio899@gmail.com>
2019-07-08 17:35:08 -0400

    init sw test

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,26 @@ if (navigator.serviceWorker) {
 	console.log('resource si se puede implementar el serviceWorker');
 }
 
+self.addEventListener('install', event => {
+	console.log('Instalando SW...');
+	const installation = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			console.log('instalaciones terminadas');
+			self.skipWaiting();
+			resolve();
+		},1000);
+	});
+	event.waitUntil(installation);
+});
+
+self.addEventListener('activate', event => {
+	// Borrar cache Vieja
+	console.log('SW activo...');
+
+});
+
 self.addEventListener('fetch', event => {
 	// URl de recusos Usados
 	console.log(event.request.url);
+	console.log('*** req');
 });
