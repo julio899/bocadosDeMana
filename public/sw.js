d460946c8f4e046c5e726a40b47ac5eb47e272ec
Julio Vinachi <julio899@gmail.com>
2019-07-09 21:37:39 -0400

    test Only Full cache

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,20 @@
 if (navigator.serviceWorker) {
 	navigator.serviceWorker.register('/sw.js');
 	console.log('resource si se puede implementar el serviceWorker');
+
+/*	caches.has('static').then(()=>{
+			// si no existe se crea el espacio de cache
+			caches.open('static').then(cache => {
+				cache.addAll([
+					'/img/bg2.jpg'
+				]);
+			});
+
+	});*/
+
 }
+
+
 
 self.addEventListener('install', event => {
 	console.log('Instalando SW...');
@@ -22,7 +35,43 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+
 	// URl de recusos Usados
-	console.log(event.request.url);
-	console.log('*** req');
+	// console.log(event.request.url);
+	// caches.open('static').then(cache => {
+	// 	cache.match(event.request.url)
+	// 		.then((r)=>{
+	// 				console.log(r.ok,event.request.url);
+	// 				if(!r.ok){
+	// 					cache.add(event.request.url)
+	// 				}
+	// 			});
+	// });
+
+	// ------
+	let req = event.request;
+	let url = event.request.url;
+	
+	let buffer = null;
+	let existe = caches.open('static')
+	.then(cache => {
+		cache.match(url)
+			.then((r)=>{
+				return ( r === undefined ) ? false:true;
+			})
+			.then((respuesta)=>{
+				if(!respuesta && url.indexOf('chrome-extension')!==0 && url.indexOf('inject.js')===-1 )
+				{
+					// si no esta en el cache que lo agregue
+	 				cache.add(url);
+				}
+
+			});			
+	});
+		event.waitUntil(existe);			
+	if ( url.indexOf('chrome-extension')!==0 && url.indexOf('inject.js')===-1 ) {
+		return event.respondWith( caches.match(event.request) );	
+	}
+	
+	
 });
