ef6ae2439a4418700016ca327fecbe7a329432da
Julio Vinachi <jvinachi@strappinc.com>
2019-10-28 09:36:50 -0400

    test token

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use \Firebase\JWT\JWT;
 class FirebaseController extends Controller{
 
 	function login(Request $request){
-		return $request;
+		$jwt = JWT::encode($request, 'bocados');
+		return array('t' => $jwt );
 	}
 
 }
