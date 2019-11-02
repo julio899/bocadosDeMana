13a8a576902a214c57f33d99fe4f2c7db8d9edd3
Julio Vinachi <jvinachi@strappinc.com>
2019-11-02 11:42:40 -0400

    Activando Cors

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -5,8 +5,12 @@ use \Firebase\JWT\JWT;
 class FirebaseController extends Controller{
 
 	function login(Request $request){
-		$jwt = JWT::encode($request, 'bocados');
-		return array('t' => $jwt );
+		// $jwt = JWT::encode($request, 'bocados');
+        $parametros = $request->route()->parameters();
+		return array(
+        	'usuario' => $request->input('usuario'),
+        	'clave' => $request->input('clave')
+		);
 	}
 
 }
